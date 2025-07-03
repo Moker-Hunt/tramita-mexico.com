37d77aea5999d0b224bfbaa167399adae5083e49
Moker-Hunt <92677140+Moker-Hunt@users.noreply.github.com>
2025-07-03 18:27:30 +0500

    1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './*.html',
+    './**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+        }
+      }
+    },
+  },
+  plugins: [],
+} 
